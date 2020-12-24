e503d00fd15ee27a6ec133d0257639f01278a1ee
7UR7L3 <m7ur7l3@yahoo.com>
2020-12-24 01:19:47 -0700

    added epub

--- manualToEpub.ps1
+++ manualToEpub.ps1
@@ -3,5 +3,5 @@ pandoc `
     --filter ..\fix-links-single-file.py `
     -s $(cat ..\pandocIncludes.txt) `
     -f markdown-raw_tex `
-    -t html5 `
-    -o ..\AnkiManual-20201222.html
+    -t epub `
+    -o ..\AnkiManual-20201222.epub
